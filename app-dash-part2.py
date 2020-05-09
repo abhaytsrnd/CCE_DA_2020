baf3843692689bde672731a7b026606ba136d472
Abhay Kumar <49036360+abhaytsrnd@users.noreply.github.com>
2020-05-09 13:02:38 +0530

    Add files via upload

--- app-dash-part2.py
+++ app-dash-part2.py
@@ -9,6 +9,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_table
 
+import plotly.graph_objs as go
+
 import pandas as pd
 
 from dataanalytics.stats_linear_regression.linear_regression import LinearRegression
@@ -18,6 +20,9 @@ df_original = pd.read_csv('.\data\original_data.csv')
 df_original = df_original[df_original.columns[1:]]
 df_cleaned = pd.read_csv('.\data\cleaned_df.csv')
 df_cleaned = df_cleaned[df_cleaned.columns[1:]]
+layout = go.Layout(title = 'Actual vs Predicted Y Plot',
+                   hovermode = 'closest')
+fig = go.Figure(data = [], layout = layout)
 
 def generate_table(dataframe, max_rows=5):
     return html.Table([
@@ -52,7 +57,9 @@ app.layout = html.Div(children=[
     generate_table(df_cleaned) ],
     style={'width': '78%', 'display': 'inline-block'}),
     html.Hr(),
-
+    
+    html.H3(children='Variable Selection and Plotting'),
+    
     html.Div([
         html.Div([
             html.Label('Select X variable from Dropdown'),
@@ -95,21 +102,34 @@ app.layout = html.Div(children=[
             dcc.Graph(id='scatter-plot'),
         ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),        
     ]),
+    html.Hr(),
     
     html.H3('Statistics Summary Table'),
-    html.Table(id='stats_table'),
+    html.Div([
+        html.Div([
+            html.Table(id='stats_table'),
+        ], style={'width': '60%', 'display': 'inline-block'}),
+        
+        html.Div([
+            html.Label('Linear Regression Coefficients'),
+            html.Table(id='coeff_table'),
+        ],style={'width': '30%', 'float': 'right', 'display': 'inline-block'}),
+    ]),
+        
     html.Hr(),
     
     html.Div([
+        html.H3('Linear Regression'),
         html.Div([
-            html.H4('Linear Regression Coefficients'),
-            html.Table(id='coeff_table'),
-        ],style={'width': '40%', 'display': 'inline-block'}),
+            dcc.Graph(id='lr-plot', figure=fig),
+        ], style={'width': '60%', 'display': 'inline-block'}),
         
+        
+      
         html.Div([
-            html.H4('Linear Regression ANOVA Table'),
+            html.Label('ANOVA Table'),
             html.Table(id='linear_anova_table'),
-            ], style={'width': '60%', 'float': 'right', 'display': 'inline-block'}),        
+            ], style={'width': '30%','float': 'right', 'display': 'inline-block'}),    
     ]),
     html.Hr(),
     
@@ -151,14 +171,15 @@ def scatter_plot(x_var_value, y_var_value):
                 'title': y_var_value,
                 'type': 'linear'
             },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
+            margin={'l': 30, 'b': 30, 't': 10, 'r': 0},
+            hovermode='closest',
+            height=300,  # px
         )
     }
     
-@app.callback(Output('stats_table', 'children'), 
+@app.callback([Output('stats_table', 'children'), Output('coeff_table', 'children'), Output('lr-plot','figure')], 
             [Input('ordered-df', 'children')])
-def update_stats_table(json_ordered_data):
+def stats_table_and_linear_regression(json_ordered_data):
     dff = pd.read_json(json_ordered_data, orient='split')
     col = list(dff.columns)
     y = list(dff[col[-1]])
@@ -171,36 +192,39 @@ def update_stats_table(json_ordered_data):
 
     model = LinearRegression()
     (summary, params, ycap) = model.fit(data, y)
+    #summary = [ '%.4f' % elem for elem in summary ]
+    for dict_value in summary:
+        for k, v in dict_value.items():
+            dict_value[k] = round(v, 4)
     df_stats = pd.DataFrame(summary)
     df_stats['Var_Name'] = col 
     df_stats = df_stats[['Var_Name','count','min','max','mean','variance','std','covariance','r']]
     #print(summary)
-    table = generate_table(df_stats, len(col))
-    return table
+    table1 = generate_table(df_stats, len(col))
     
-@app.callback(Output('coeff_table', 'children'), 
-            [Input('ordered-df', 'children')])
-def update_stats_table(json_ordered_data):
-    dff = pd.read_json(json_ordered_data, orient='split')
-    col = list(dff.columns)
-    y = list(dff[col[-1]])
-    data = []
-    x_col = col[:-1]
-    data = [[] for i in range(len(x_col))]
-    for i in range(len(x_col)):
-        x = dff[x_col[i]].values.tolist()
-        data[i] = x
-
-    model = LinearRegression()
-    (summary, params, ycap) = model.fit(data, y)
     x_col.append('Constant')
-    print('param col:',x_col)
-    print('param:',params)
+    params = [ '%.4f' % elem for elem in params ]
     df_coeff = pd.DataFrame(params, columns=['Coefficient'])
     df_coeff['Var_Name'] = x_col 
     df_coeff = df_coeff[['Var_Name','Coefficient']]
-    table = generate_table(df_coeff, len(x_col))
-    return table
-
+    table2 = generate_table(df_coeff, len(x_col))
+    
+    trace_1 = go.Scatter(x = list(range(len(y))), y = ycap,
+                    name = 'Y_Predicted',
+                    line = dict(width = 2,
+                                color = 'rgb(229, 151, 50)'))
+    trace_2 = go.Scatter(x = list(range(len(y))), y = y,
+                        name = 'Y_Actual',
+                        line = dict(width = 2,
+                                    color = 'rgb(106, 181, 135)'))   
+    ydiff = [y[i] - ycap[i] for i in range(len(y))]
+    trace_3 = go.Scatter(x = list(range(len(y))), y = ydiff,
+                        name = 'Y_Error',
+                        line = dict(width = 2,
+                                    color = 'rgb(236, 10, 15)'))                                   
+    fig = go.Figure(data = [trace_1, trace_2, trace_3], layout = layout)
+    
+    return table1, table2, fig
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
